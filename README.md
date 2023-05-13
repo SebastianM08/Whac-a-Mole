bf00b5154297fc563ec4c495ca07a708815262b3
SebastianM08 <2puntoscreativo@gmail.com>
2023-05-13 08:54:10 -0700

    Last commit?

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Whack-a-Mole 
+
+Whack a mole is a very simple game where you have nine tiles and in each tile there is a hole where the mole can appear and the goal is to whack them all by clicking on it. 
+I decided to make the game a bit more fun by making it a Pokemon themed game.
+For each hole we have a pokeball, as the mole we have pikachu and also we have diglett 
+The goal is to click on Pikachu every time he appears from the pokeball and avoid clicking on Diglett or you'll be out of luck.
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let currDiglettTile;
 let currPikachuTile;
-
+let score = 0;
+let gameOver = false;
 
 window.onload = function() {
     setGame();
@@ -12,12 +13,14 @@ function setGame() {
         //<div id="0-8"></div>
         let tile = document.createElement("div");
         tile.id = i.toString();
+        tile.addEventListener("click", selectTile);
         document.getElementById("board").appendChild(tile); 
     }
 
     setInterval(setDiglett, 1000); //1000 milliseconds = 1 second, every 1 second call setDiglett  
     setInterval(setPikachu, 2000); //2000 milliseconds = 2 seconds, every 2 second call setPikachu
 }
+
 
 function getRandomTile () {
     // math.random --> (0-1) * 9 = (0-9) --> round down to (0-9) integers 
@@ -26,6 +29,9 @@ function getRandomTile () {
 }
 
 function setDiglett() {
+    if(gameOver) {
+        return;
+    }
 
     if (currDiglettTile) {
         currDiglettTile.innerHTML = "";
@@ -35,11 +41,17 @@ function setDiglett() {
     diglett.src = "./diglett.png";
 
     let num = getRandomTile();
+    if (currPikachuTile && currPikachuTile.id == num) {
+        return;
+    }
     currDiglettTile = document.getElementById(num);
     currDiglettTile.appendChild(diglett); 
 }
 
 function setPikachu() {
+    if(gameOver) {
+        return;
+    }
 
     if (currPikachuTile) {
         currPikachuTile.innerHTML = "";
@@ -49,6 +61,36 @@ function setPikachu() {
     pikachu.src = "./pikachu.png"; 
 
     let num = getRandomTile();
+    if (currDiglettTile && currDiglettTile.id == num) {
+        return;
+    }
     currPikachuTile = document.getElementById(num);
-    currPikachuTile.appendChild(pikachu);
+    currPikachuTile.appendChild(pikachu); 
 }
+
+function selectTile() {
+    if(gameOver) {
+        return;
+    }
+
+    if (this == currPikachuTile) {
+        score += 100;
+        document.getElementById("score").innerText = "Score: " + score.toString(); //score update 
+    }
+    else if (this == currDiglettTile) {
+        document.getElementById("score").innerText = "GAME OVER: " + score.toString();
+        gameOver = true;    
+    }
+}
+
+//Restart Game
+const restartButton = document.getElementById("restart");
+//Add event listener to restart button
+restartButton.addEventListener("click", () => {
+    //Reset Variables
+     currDiglettTile;
+     currPikachuTile;
+     score = 0; 
+     gameOver = false; 
+    //Reset Game board 
+}); 
